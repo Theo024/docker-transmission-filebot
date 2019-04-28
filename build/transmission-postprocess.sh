c4d069ec52cf64a100fd42985d5592b68c94b76d
Théo Lévesque <theo.levesque.024@gmail.com>
2019-04-28 13:13:01 +0200

    Une Kodi naming format

--- build/transmission-postprocess.sh
+++ build/transmission-postprocess.sh
@@ -8,4 +8,4 @@ ARG_LABEL="N/A"
 # Configuration
 CONFIG_OUTPUT="/library"
 
-filebot -script fn:amc --output "$CONFIG_OUTPUT" --action move --conflict skip -non-strict --log-file amc.log --def clean=y excludeList=".excludes" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
+filebot -script fn:amc --output "$CONFIG_OUTPUT" --action move --conflict skip -non-strict --log-file amc.log --def movieFormat="{kodi}" seriesFormat="{kodi}" clean=y excludeList=".excludes" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
